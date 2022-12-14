07ce8ceff72f71b656d2d335a5af85a92062ab65
scottk <templarsigismund@gmail.com>
2022-12-13 22:44:47 -0700

    run file

---
+++ Neural Networks/runNN.sh
@@ -0,0 +1,4 @@
+python 3LayerNN.py
+#python3 3LayerNN.py
+#if python is not working for you for some reason run just python3, second commented line.
+#uncomment plot lines of with "UNCOMMENT FOR PLOT" to get output of plot, will not work on command line in CADE
